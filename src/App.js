22feb46d55f42e7509487d2c92bf94d5d242e730
DiogoZ <diogozang2@gmail.com>
2021-07-29 23:44:39 -0300

    components separated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import styles from './App.css';
+import styles from './App.scss';
+
+import Box from './components/Box';
+import Icon from './components/Icon';
 
 
 
@@ -8,28 +11,33 @@ const Wrapper = styled.div`
   display: block;
   overflow: hidden;
   background-color: #fff;
-  margin-top: 3rem;
+
+  margin: 2rem auto;
   width: 450px;
-  aspect-ratio: 1/1.8;
+  aspect-ratio: 9/16;
   border: 1rem #222 solid;
   border-radius: 2.5rem;
+
+  @media (max-width:450px){
+    width:100%;
+    border:none;
+  }
 `;
 
 const Screen = styled.div``;
 const Header = styled.div``;
-const Icon = styled.div``;
 const Main = styled.div``;
 const Content = styled.div``;
-const Box = styled.div``;
 const Footer = styled.div``;
 
 function App() {
 
-  let account = {
+  const account = {
     id:'5824',
     agency:'55852-8',
-    balance:0,
-    savings:0,
+    balance:2800.35,
+    savings:500,
+    investments:1500,
     bclass:'A',
 
     addToBalance: function(toAdd) {
@@ -59,6 +67,7 @@ function App() {
     account: account.id,
   }
 
+  document.title = `Olá, ${user.name}`;
 
   return (
     <>
@@ -68,37 +77,25 @@ function App() {
               <div className="header-top row">
                 <div className="welcome">
                   Olá, {user.name}! <br/>
-                  {account.id} / {account.agency}
+                  {account.id} / {account.agency} {account.bclass}
                 </div>
                 <div className="menu">☰</div>
               </div>
 
               <div className="header-bottom">
                 <Content className="content-row">
-                  <Icon className="square-icon">
-                    <div className="icon-icon">$</div>
-                    <div className="icon-title">pagar</div>
-                  </Icon>
-                  <Icon className="square-icon">
-                    <div className="icon-icon">$</div>
-                    <div className="icon-title">transferir</div>
-                  </Icon>
-                  <Icon className="square-icon">
-                    <div className="icon-icon">$</div>
-                    <div className="icon-title">pix</div>
-                  </Icon>
-                  <Icon className="square-icon">
-                    <div className="icon-icon">$</div>
-                    <div className="icon-title">extrato</div>
-                  </Icon>
+                  <Icon type="pagar" />
+                  <Icon type="transferir" />
+                  <Icon type="pix" />
+                  <Icon type="extrato" />
                 </Content>
               </div>
           </Header>
           <Main className="main">
             <div className="content-col">
-              <Box className="content-box"><h2>Conta Corrente</h2></Box>
-              <Box className="content-box"><h2>Poupança</h2></Box>
-              <Box className="content-box"><h2>Investimentos</h2></Box>
+              <Box title="Conta Corrente" value={parseFloat(account.balance).toFixed(2)}/>
+              <Box title="Poupança" value={parseFloat(account.savings).toFixed(2)}/>
+              <Box title="Investimentos" value={parseFloat(account.investments).toFixed(2)}/>
             </div>
           </Main>
           <Footer className="footer">
